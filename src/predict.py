5ecbedf0aa14b03e32f846315f8a09a5f72560fa
Prakhar Shrivastava <darshilshrivastava5@gmail.com>
2026-03-10 13:14:59 +0530

    Add custom threshold for churn prediction and fix scaler path

--- src/predict.py
+++ src/predict.py
@@ -1,30 +1,37 @@
 import pickle
 import numpy as np
-
-# Load model
-import pickle
 import os
 
-# Get project root directory
+# Project root
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Build model path
+# Model paths
 model_path = os.path.join(BASE_DIR, "models", "churn_model.pkl")
+scaler_path = os.path.join(BASE_DIR, "models", "scaler.pkl")
 
 # Load model
 model = pickle.load(open(model_path, "rb"))
-scaler = pickle.load(open("models/scaler.pkl", "rb"))
+scaler = pickle.load(open(scaler_path, "rb"))
 
 
 def predict_churn(recency, frequency, monetary, avg_order_value, avg_review_score):
 
+    # Create feature array
     features = np.array([[recency, frequency, monetary,
                           avg_order_value, avg_review_score]])
 
+    # Scale features
     features_scaled = scaler.transform(features)
 
-    prediction = model.predict(features_scaled)[0]
-
+    # Get churn probability
     probability = model.predict_proba(features_scaled)[0][1]
+
+    # Custom threshold (important)
+    threshold = 0.30
+
+    if probability > threshold:
+        prediction = 1
+    else:
+        prediction = 0
 
     return prediction, probability
